293b8aaa26fc257975a84cb3508c97a12d072c9e
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-09-03 22:30:57 +1000

    add get history price effect

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,26 +1,70 @@
 import React, { useEffect, useState } from "react";
-import { getAllCoins } from "../services/coin-services";
+import { getAllCoins, getCoinPrices } from "../services/coin-services";
 import AllCoinChart from "../components/AllCoinChart/AllCoinChart";
+import TimeChart from "../components/TimeChart/TimeChart";
 
 const MainPage = () => {
+  const [loading, setLoading] = useState(true);
+
   const [allCoins, setAllCoins] = useState();
   const [requestAllCoins, setRequestAllCoins] = useState(0);
+
+  const selectedCoins = ["bitcoin", "ethereum", "tether"];
+  const [coinsPrice, setCoinsPrice] = useState();
+  const [requestCoinPrice, setRequestCoinPrice] = useState(0);
 
   useEffect(() => {
     getAllCoins()
       .then((coins) => {
         setAllCoins(coins);
+        setLoading(false);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => console.error("Error fetching allCoins:", err));
   }, [requestAllCoins]);
 
-  console.log(allCoins);
+  useEffect(() => {
+    const fetchCoinPrices = async () => {
+      const updatedCoinsPrice = {};
+      for (let i = 0; i < selectedCoins.length; i++) {
+        try {
+          const coinPrice = await getCoinPrices(selectedCoins[i]);
+          updatedCoinsPrice[selectedCoins[i]] = coinPrice;
+          setLoading(false);
+        } catch (err) {
+          console.error("Error fetching coinPrices:", err);
+        }
+      }
+      setCoinsPrice(updatedCoinsPrice);
+    };
+
+    fetchCoinPrices();
+  }, [requestCoinPrice]);
+
+  const makeCoinTimeRequest = () => setRequestCoinPrice(requestCoinPrice + 1);
+
+  // console.log(allCoins);
+
+  // console.log(coinsPrice);
 
   return (
     <div>
       <h1>Charts of crypto coins</h1>
       <p style={{ fontStyle: "italic" }}>Data from coincap.io</p>
-      {allCoins && <AllCoinChart coins={allCoins} />}
+      <hr />
+      {loading || !allCoins ? (
+        <p>Loading...</p>
+      ) : (
+        <AllCoinChart coins={allCoins} />
+      )}
+      <hr />
+      {loading || !coinsPrice ? (
+        <p>Loading...</p>
+      ) : (
+        <TimeChart
+          coinsPrice={coinsPrice}
+          makeCoinTimeRequest={makeCoinTimeRequest}
+        />
+      )}
     </div>
   );
 };
