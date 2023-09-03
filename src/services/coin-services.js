7aeaa85c59bc7abcd5494b7eaeeb58b57a83987d
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-09-03 22:26:35 +1000

    add get coin history price service

--- src/services/coin-services.js
+++ src/services/coin-services.js
@@ -10,3 +10,20 @@ export const getAllCoins = async () => {
   const data = await response.json();
   return data.data;
 };
+
+export const getCoinPrices = async (coin) => {
+  const response = await fetch(
+    `https://api.coincap.io/v2/assets/${coin}/history?interval=d1`,
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+  if (!response.ok) {
+    // console.log(response);
+    throw new Error(`Failed to fetch history price for coin ${coin}`);
+  }
+  const data = await response.json();
+  return data.data;
+};
